c4fe7e3223630958705d5c538c6c7404b3a86416
kiranikram <kiran.ikram91@gmail.com>
2020-07-03 10:53:55 +0100

    implementing cnet hanabi

---
+++ hanabi_learning_environment/agents/DIAL/cnet.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import functools
+
+import gin.torch
+import numpy as np
+
+### C-net RNN GRU
