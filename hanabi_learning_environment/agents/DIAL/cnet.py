342b9e85e20b30452d790d192f18500888da14ce
kiranikram <kiran.ikram91@gmail.com>
2020-07-03 19:52:50 +0100

    implementing cnet hanabi

--- hanabi_learning_environment/agents/DIAL/cnet.py
+++ hanabi_learning_environment/agents/DIAL/cnet.py
@@ -3,8 +3,24 @@ from __future__ import division
 from __future__ import print_function
 
 import functools
+import numpy as np
+import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn import functional as F
+from torch.autograd import Variable
+from torch.nn.utils import clip_grad_norm_
+
+import matplotlib.pyplot as plt
 
 import gin.torch
 import numpy as np
 
-### C-net RNN GRU
+### C-net DRQN
+
+class CNet(nn.Module):
+    def __init__(self,
+                 num_players = None):
+
+
