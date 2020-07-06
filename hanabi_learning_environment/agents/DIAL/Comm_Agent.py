081221ba78a23bba70358629fd619e759e601302
kiranikram <kiran.ikram91@gmail.com>
2020-07-06 07:27:03 +0100

    agent class for DIAL structuring

---
+++ hanabi_learning_environment/agents/DIAL/Comm_Agent.py
@@ -0,0 +1,171 @@
+# Import libraries
+import numpy as np
+import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn import functional as F
+from torch.autograd import Variable
+from torch.nn.utils import clip_grad_norm_
+
+import matplotlib.pyplot as plt
+
+ #TODO agent no representation in HLE ; could be current_player
+
+ #TODO differentiate between num_actions and legal moves, as compared to game_action_space & game_action_space total
+ ### could be num_actions & num_actions + 1
+ ### DIAL adds a one bit action for comms action ; Double Check!
+
+ #TODO get_action_range in the format of HLE rl env
+ #TODO note from examples where to pass in env for agent
+
+ #TODO configs should contain train / eval step range HLE
+class Agent:
+
+    def __init__(self,
+                 model,
+                 target,
+                 agent_no,
+                 learning_rate = 0.0005,
+                 game_comm_sigma = 2,
+                 game_comm_bits = 1,
+                 momentum = 0.05,
+                 batch_size = 32,
+                 current_player,
+                 num_actions,
+                 gamma = 1,
+                 step_target = 100
+                 ):
+
+        self.model = model
+        self.model_target = target
+        self.learning_rate = learning_rate
+        self.id = current_player
+        self.num_actions = num_actions
+        self.num_actions_total = num_actions + 1
+
+
+
+        for param in target.parameters():
+            param.requires_grad = False
+
+        self.episodes = 0
+        self.dru = DRU(self.game_comm_sigma)
+        self.optimizer = optim.RMSprop(
+            params = model.get_params(), lr=self.learning_rate, momentum=self.momentum)
+
+    def reset(self):
+
+        self.model.reset_parameters()
+        self.model_target.reset_parameters()
+        self.episodes = 0
+
+    #CBT visavis rationale
+    def _eps_flip(self,eps):
+        return np.random.rand(self.batch_size) < eps
+
+    #CBT viasvis usage rationale
+    def _random_choice(self , items):
+        return torch.from_numpy(np.random.choice(items,1)).item()
+
+    def select(self, step , q , eps = 0 , target =False, train = False):
+
+        if not train:
+            eps = 0
+
+        #CBT : action range (clue:step function)
+        action_range, comm_range = self.environment.get_action_range(step,self.id)
+
+        action = torch.zeroes(self.batch_size , dtype = torch.long)
+        action_value = torch.zeroes(self.batch_size)
+        comm_vector = torch.zeroes(self.batch_size, self.game_comm_bits)
+
+        select_random_a = self._eps_flip(eps)
+        for b in range(self.batch_size):
+            q_a_range = range(0, self.num_actions)
+
+            a_range = range(action_range[b,0].iten() - 1, action_range[b,1].item())
+
+            if select_random_a[b]:
+
+                action[b] = self._random_choice(a_range)
+                action_value[b] = q[b , action[b]]
+
+            else:
+                action_value[b] , action[b] = q[b,a_range].max(0)
+                action[b] = action[b] + 1
+
+            q_c_range = range(self.num_actions, self.num_actions_total)
+
+            if comm_range[b,1] > 0:
+                c_range = range(comm_range[b,0].item()-1,comm_range[b,1].item())
+                comm_vector[b] = self.dru.forward(q[b,q_c_range] , train_mode = train)
+
+        return(action,action_value) ,comm_vector
+
+    def get_loss(self,episode):
+
+        total_loss = torch.zeroes(self.batch_size)
+        for b in range (self.batch_size):
+            b_steps = episode.steps[b].item()
+            for step in range(b_steps):
+                record = episode.step_records[step]
+                for i in range(self.num_players):
+                #Look into: i should refer to current player
+                    td_action = 0
+                    r_t = record.r_t[b][i]
+                    q_a_t = record.q_a_t = record.q_a_t[b][i]
+
+                    if record.a_t[b][i].item() > 0:
+                        td_action = r_t - q_a_t
+                    else:
+                        next_record = episode.step_records[step + 1]
+                        q_next_max = next_record.q_a_max_t[b][i]
+                        td_action = r_t = self.gamma * q_next_max - q_a_t
+
+                    loss_t = td_action ** 2
+                    total_loss[b] = total_loss[b] + loss_t
+
+        loss = total_loss.sum()
+        return loss / (self.batch_size * self.num_players)
+
+    def update(self,episode):
+
+        self.optimizer.zero_grad()
+        loss = self.get_loss(episode)
+        loss.backward()
+
+        clip_grad_norm_(parameters = self.model.get_params(), max_norm=10)
+        self.optimizer.step()
+        self.episodes += 1
+
+        if self.episodes % self.step_target == 0:
+            self.model_target.load_state_dict(self.model.state_dict())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- hanabi_learning_environment/agents/DIAL/cnet.py
+++ hanabi_learning_environment/agents/DIAL/cnet.py
@@ -17,10 +17,111 @@ import matplotlib.pyplot as plt
 import gin.torch
 import numpy as np
 
-### C-net DRQN
 
-class CNet(nn.Module):
-    def __init__(self,
-                 num_players = None):
+def weight_reset(m):
+    if isinstance(m, nn.Batchnorm1d) or isinstance(m , nn.Linear):
+        m.reset_parameters()
+"""
+Inputs: tuple of (o_t^a, m_t-1^a', u_t-1^a, a ; 
+a: player who's turn it is
+ """
+ # TODO: add gin configurable ; replace eg num_players with configuarble version
+ # TODO: current testing is with comms; option a and b; to be replaced with Batesian belief update
+class DRQNet(nn.Module):
+
+    def __init__(self, num_players, rnn_size, comm_size, num_actions , observation_size , init_param_range ):
+
+        super(DRQNet, self).__init__()
+        self.num_players = 3
+        self.rnn_size = 128
+        self.comm_size = 2
+        self.num_actions = num_actions
+        self.observation_size = observation_size
+        self.init_param_range = (-0.08, 0.08)
+
+        #Embedding matrix for DRQN
+        self.action_matrix = nn.Embedding(self.num_players,self.rnn_size)
+        self.observation_matrix = nn.Embedding(self.observation_size,self.rnn_size)
+        self.previous_action_matrix = nn.Embedding(self.num_actions,self.rnn_size)
+
+        #Single layer NN for producing embeddings for messages
+        self.message = nn.Sequential(
+            nn.BatchNorm1d(self.comm_size),
+            nn.Linear(self.comm_size, self.rnn_size),
+            nn.ReLu(inplace=True)
+        )
+
+        #RNN component for history over POMDP
+        self.rnn = nn.GRU(input_size=self.rnn_size, hidden_size=self.rnn_size,num_layers=2,batch_first=True)
+
+        #Output from RNN layer
+        self.output = nn.Sequential(
+            nn.Linear(self.rnn_size, self.rnn_size),
+            nn.BatchNorm1d(self.rnn_size),
+            nn.ReLu(),
+            nn.Linear(self.rnn_size,self.observation_size)
+        )
+
+    def retrieve_parameters(self):
+        return list(self.parameters())
+
+    def reset_parameters(self):
+
+        self.rnn.reset_parameters()
+        self.action_matrix.reset_parameters()
+        self.observation_matrix.reset_parameters()
+        self.previous_action_matrix.reset_parameters()
+        self.message.apply(weight_reset)
+        self.output.appky(weight_reset)
+        for x in self.rnn.parameters():
+            x.data.uniform_(*self.init_param_range)
+
+    # TODO check reference for observation vs state
+    # TODO assert via HLE agent rotations (ie cannot be random) how is offset determined
+    def forward(self, observation,messages, hidden_state, previous_action ,agent):
+
+        observation = Variable(torch.LongTensor(observation))
+        hidden_state = Variable(torch.LongTensor(hidden_state))
+        previous_action = Variable(torch.LongTensor(previous_action))
+        agent = Variable(torch.LongTensor(agent))
+
+        Z_A = self.action_matrix(agent)
+        Z_O = self.state_matrix(observation)
+        Z_U = self.observation_matrix(previous_action)
+        Z_M = self.message(messages.view(-1,self.comm_size))
+
+        #Element wise summation of embeddings
+
+        Z = Z_A + Z_O + Z_U + Z_M
+        Z = Z.unsqueeze(1)
+
+        rnn_out , H = self.rnn(Z, hidden_state)
+
+        #Retrieve final QNet output Q values from GRU
+        OUT = self.output(rnn_out[:,-1,:].squeeze())
+
+        return H, OUT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
